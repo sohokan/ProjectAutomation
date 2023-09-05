127715e9084f307679dbd5cdd388f92915719c29
MAS <mas@gmail.com>
2023-09-05 13:30:38 -0600

    added tc 8

--- src/test/java/com/ti/pompages/ProductPage.java
+++ src/test/java/com/ti/pompages/ProductPage.java
@@ -11,12 +11,18 @@ public class ProductPage extends HomePage {
 
     By productsLocator =By.xpath("//div[@class='productinfo text-center']");
 
+    By viewProductLocator=By.cssSelector("a[href *='/product_details/']");
+
     WebElement linkProductsPage;
 
     List<WebElement> productItems;
 
+    List<WebElement> linkViewProducts;
+
     public void VerifyProductList()
     {
+        int countViewProducts=0;
+
         linkProductsPage=driver.findElement(productsPageLocator);
         linkProductsPage.click();
         try {
@@ -28,9 +34,27 @@ public class ProductPage extends HomePage {
         if(driver.getCurrentUrl().contains("google_vignette"))
             linkProductsPage.click();
 
+        productItems=driver.findElements(productsLocator);
+
+        System.out.println("Total Items: "+productItems.size());
+
+        linkViewProducts=driver.findElements(viewProductLocator);
+
+        for (var item:productItems) {
 
 
+            System.out.println(item.findElement( By.tagName("h2")).getText());
+            System.out.println(item.findElement( By.tagName("p")).getText());
+            System.out.println(item.findElement( By.xpath("//a[contains(text(),'Add to cart')]")).getText());
 
+            System.out.println(linkViewProducts.get(countViewProducts).getText());
+            countViewProducts++;
+
+
+        }
+//verify click 1st
+//productItems.get(0).findElement( By.xpath("//a[contains(text(),'Add to cart')]")).click();
+        linkViewProducts.get(1).click();
 
 
 
